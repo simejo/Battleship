4483d5428280394fe4773159f7d23c543f804b35
siljec <siljechristensen92@gmail.com>
2015-03-11 15:33:58 +0100

    Board.java: Add constructor and getter for board. BoardValues: Add different board values

--- app/src/main/java/a8/battleship/Logic/BoardValues.java
+++ app/src/main/java/a8/battleship/Logic/BoardValues.java
@@ -4,4 +4,15 @@ package a8.battleship.Logic;
  * Created by majakirkerod on 11.03.15.
  */
 public enum BoardValues {
+    EMPTY,
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    MIDDLE,
+    NORTH_DESTROYED,
+    SOUTH_DESTROYED,
+    EAST_DESTROYED,
+    WEST_DESTROYED,
+    MIDDLE_DESTROYED
 }
