7310fb6ccbf00654c5584375ee08cd95ae137ba2
majakirkeroed <majakirkeroed@gmail.com>
2015-03-11 15:20:54 +0100

    added enum values

--- app/src/main/java/a8/battleship/Logic/BoardValues.java
+++ app/src/main/java/a8/battleship/Logic/BoardValues.java
@@ -4,4 +4,16 @@ package a8.battleship.Logic;
  * Created by majakirkerod on 11.03.15.
  */
 public enum BoardValues {
+    EMPTY,
+    NORTH,
+    SOUTH,
+    MIDDLE,
+    EAST,
+    WEST,
+    NORTHDESTROYED,
+    SOUTHDESTROYED,
+    MIDDLEDESTROYED,
+    EASTDESTROYED,
+    WESTDESTROYED
+
 }
