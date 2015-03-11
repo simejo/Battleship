5314ced47f8a243116ba0748b130dabae4d345c7
majakirkeroed <majakirkeroed@gmail.com>
2015-03-11 18:08:02 +0100

    made variables static

--- app/src/main/java/a8/battleship/Logic/Constants.java
+++ app/src/main/java/a8/battleship/Logic/Constants.java
@@ -4,14 +4,8 @@ package a8.battleship.Logic;
  * Created by majakirkerod on 11.03.15.
  */
 public class Constants {
-    private int boardSize = 10;
-    private int screenSize;
+    private static int boardSize = 10;
+    private static int screenSize;
 //I think we should make the variables static, in stead of making getters and setters for all of them
-    public int getBoardSize(){
-        return boardSize;
-    }
 
-    public void setBoardSize(int n){
-        boardSize=n;
-    }
 }
