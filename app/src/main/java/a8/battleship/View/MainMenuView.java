e7d78aad6eb1beedc2bce263d799f7b9294bd082
Simen <simen.johnsrud92@gmail.com>
2015-03-11 15:44:29 +0100

    Added onclicklistener in mainMenuView

--- app/src/main/java/a8/battleship/View/MainMenuView.java
+++ app/src/main/java/a8/battleship/View/MainMenuView.java
@@ -1,7 +1,63 @@
 package a8.battleship.View;
 
-/**
- * Created by siljechristensen on 11.03.15.
- */
-public class MainMenuView {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import a8.battleship.R;
+import a8.battleship.View.CreditsView;
+
+public class MainMenuView extends ActionBarActivity implements View.OnClickListener{
+
+    private Button startButton, settingsButton, creditsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_view);
+
+        startButton = (Button) findViewById(R.id.buttonStart);
+        settingsButton = (Button) findViewById(R.id.buttonSettings);
+        creditsButton = (Button) findViewById(R.id.buttonStart);
+
+        startButton.setOnClickListener(this);
+        settingsButton.setOnClickListener(this);
+        creditsButton.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v){
+        if(v.getId() == R.id.buttonCredits){
+            Log.i("in on click","credit");
+            startActivity(new Intent(MainMenuView.this, CreditsView.class));
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
