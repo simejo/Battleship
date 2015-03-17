540f697927a700401697c4d468e65f4710f2a790
Simen <simen.johnsrud92@gmail.com>
2015-03-17 14:49:19 +0100

    Added onclicklistener and redirected the button click

--- app/src/main/java/a8/battleship/View/StartUpView.java
+++ app/src/main/java/a8/battleship/View/StartUpView.java
@@ -1,9 +1,35 @@
 package a8.battleship.View;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+import a8.battleship.R;
+
 /**
  * Created by Kartefull on 11.03.2015.
  */
-public class StartUpView {
+public class StartUpView extends ActionBarActivity implements View.OnClickListener{
 
     //Where to place the boats
+
+    private Button startGame;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_view);
+
+        startGame = (Button) findViewById(R.id.buttonDone);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.buttonDone){
+            startActivity(new Intent(StartUpView.this, BattleView.class));
+        }
+    }
 }
