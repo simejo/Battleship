8bc9553fca261c83f2c60527829de2b8bd3a5ec9
siljec <siljechristensen92@gmail.com>
2015-03-19 14:03:01 +0100

    tull

--- app/src/main/java/a8/battleship/tokens/Ship.java
+++ app/src/main/java/a8/battleship/tokens/Ship.java
@@ -1,63 +1,107 @@
 package a8.battleship.Tokens;
 
 import android.app.Activity;
-import android.widget.ImageView;
-import android.view.View;
 
 import java.util.ArrayList;
 
 import a8.battleship.Logic.BoardValues;
-import a8.battleship.R;
 
 /**
  * Created by Kartefull on 11.03.2015.
  */
 public class Ship extends Activity{
-    private ArrayList<BoardValues> boat;
+    private ArrayList<BoardValues> ship;
+
+
+    /*  Need to save where the boats will be located at the board.
+    *   x and y is the top end/left end at the boat
+    *   These values will be set during the
+    *   public void randomizeShipPositions() method in Board.java
+    */
+    private int x;
+    private int y;
     private int direction;
+    private int positionInArray; //Will be added later, in the Board.java class. Makes it easier to find the ship (?)
+
+    /*  Need to control if the boat has sunk or not. If partsLeft == 0, the boat has sunk
+     *  TODO: this should be updated in BattleView.java
+     *
+     */
+    private int partsLeft;
 
     public Ship(int shipSize, int direction){//direction, 0 equals vertical, 1 equals horizontal
-        boat = new ArrayList<BoardValues>();
+        ship = new ArrayList<BoardValues>();
         this.direction=direction;
+        this.x = 0;
+        this.y = 0;
+        this.partsLeft = shipSize;
+        this.positionInArray = 0;
+
         if(direction==0){//adding pictures to array if vertical
-            boat.add(BoardValues.NORTH);
+            ship.add(BoardValues.NORTH);
+            for(int i=1; i<shipSize-2; i++){//add middle pieces
+                ship.add(BoardValues.MIDDLE_VERTICAL);
+            }
         }
         else{//if horizontal add west image
-            boat.add(BoardValues.WEST);
+            ship.add(BoardValues.WEST);
+            for(int i=1; i<shipSize-2; i++){//add middle pieces
+                ship.add(BoardValues.MIDDLE_HORIZONTAL);
+            }
         }
-        for(int i=1; i<shipSize-2; i++){//add middlepieces
-            boat.add(BoardValues.MIDDLE);
-        }
-        if(direction==0){//add endpiece
-            boat.add(BoardValues.SOUTH);
+        if(direction==0){//add end piece
+            ship.add(BoardValues.SOUTH);
         }
         else{
-            boat.add(BoardValues.EAST);
+            ship.add(BoardValues.EAST);
         }
-
     }
 
+    public void setShipPosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+
+    //the position in - private ArrayList<Ship> shipArray in Board.java
+    public int getPositionInArray(){
+        return this.positionInArray;
+    }
+
+    //the position in - private ArrayList<Ship> shipArray in Board.java
+    public void setPositionInArray(int i){
+        this.positionInArray = i;
+    }
+
+    //NOT IN USE!!!!!!!! need to fix it if we use it
+    //Placement is the position to the destroyed part in the boat - NOT IN USE, not updated with the two different MIDDLE_HORIZONTAL/VERTICAL
     public void destroyPiece(int placement){
-        if (boat.get(placement)==BoardValues.NORTH){
-            boat.set(placement, BoardValues.NORTH_DESTROYED);
+        if (ship.get(placement)==BoardValues.NORTH){
+            ship.set(placement, BoardValues.NORTH_DESTROYED);
         }
-        else if(boat.get(placement) == BoardValues.SOUTH){
-            boat.set(placement, BoardValues.SOUTH_DESTROYED);
+        else if(ship.get(placement) == BoardValues.SOUTH){
+            ship.set(placement, BoardValues.SOUTH_DESTROYED);
         }
-        else if(boat.get(placement) == BoardValues.WEST){
-            boat.set(placement, BoardValues.WEST_DESTROYED);
+        else if(ship.get(placement) == BoardValues.WEST){
+            ship.set(placement, BoardValues.WEST_DESTROYED);
         }
-        else if(boat.get(placement) == BoardValues.EAST){
-            boat.set(placement, BoardValues.EAST_DESTROYED);
+        else if(ship.get(placement) == BoardValues.EAST){
+            ship.set(placement, BoardValues.EAST_DESTROYED);
         }
         else{
-            boat.set(placement, BoardValues.MIDDLE_DESTROYED);
+            ship.set(placement, BoardValues.MIDDLE_DESTROYED);
         }
 
     }
 
-    public ArrayList<BoardValues> getBoat(){
-        return this.boat;
+    public ArrayList<BoardValues> getShip(){
+        return this.ship;
     }
 
     public int getDirection(){
