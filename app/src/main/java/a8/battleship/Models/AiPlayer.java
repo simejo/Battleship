9c6df72ea9376339e36887263c4312d744db841c
Thea Hove Johansen <thea.h.johansen@gmail.com>
2015-04-08 11:20:14 +0200

    made a simple aiPlayer

--- app/src/main/java/a8/battleship/Models/AiPlayer.java
+++ app/src/main/java/a8/battleship/Models/AiPlayer.java
@@ -1,8 +1,46 @@
 package a8.battleship.Models;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by TheaHove on 11/03/2015.
  */
 public class AiPlayer {
     //TODO: Implement AI
+    private Board board;
+    private boolean turn;
+    private ArrayList<Integer> rndPos;
+
+    public AiPlayer() {
+        this.board = null;
+        this.turn = true;
+
+        rndPos = new ArrayList<Integer>();
+        for (int i = 0; i < 100; i++) {
+            rndPos.add(i);
+        }
+        Collections.shuffle(rndPos);
+    }
+
+    public void setBoard(Board board){
+        this.board = board;
+    }
+
+    public void setTurn(boolean turn){
+        this.turn = turn;
+    }
+
+    public boolean getTurn(){
+        return turn;
+    }
+
+    public Board getBoard(){
+        return this.board;
+    }
+
+    public Integer aiPositions() {
+        return rndPos.remove(0);
+    }
 }
