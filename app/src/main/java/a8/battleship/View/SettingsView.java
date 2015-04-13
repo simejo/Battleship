bee4877a01afeaa3ab34599aaac77b60c25d0dc1
Simen <simen.johnsrud92@gmail.com>
2015-04-13 17:08:51 +0200

    Added checkboxes in settings

--- app/src/main/java/a8/battleship/View/SettingsView.java
+++ app/src/main/java/a8/battleship/View/SettingsView.java
@@ -2,19 +2,23 @@ package a8.battleship.View;
 
 
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 
 import a8.battleship.Logic.Constants;
 import a8.battleship.R;
 
-public class SettingsView extends ActionBarActivity implements View.OnClickListener{
+public class SettingsView extends ActionBarActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener{
 
     private Button startButton, mainMenuButton;
     private RadioButton boatsRButton, girlsRButton;
+    private CheckBox checkBoxMusic, checkBoxSound;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +28,24 @@ public class SettingsView extends ActionBarActivity implements View.OnClickListe
         mainMenuButton = (Button) findViewById(R.id.buttonMainMenu);
         boatsRButton = (RadioButton) findViewById(R.id.boatsRadioButton);
         girlsRButton = (RadioButton) findViewById(R.id.girlsRadioButton);
+        checkBoxMusic = (CheckBox) findViewById(R.id.checkBoxMusicSettings);
+        checkBoxSound = (CheckBox) findViewById(R.id.checkBoxSoundSettings);
 
         startButton.setOnClickListener(this);
         mainMenuButton.setOnClickListener(this);
         boatsRButton.setOnClickListener(this);
         girlsRButton.setOnClickListener(this);
 
+        checkBoxMusic.setChecked(Constants.cbBooleanMusic);
+        checkBoxSound.setChecked(Constants.cbBooleanSound);
+
+        checkBoxSound.setOnCheckedChangeListener(this);
+        checkBoxMusic.setOnCheckedChangeListener(this);
+
     }
     @Override
     public void onClick(View v){
+
         if(v.getId() == R.id.buttonMainMenu){
             startActivity(new Intent(SettingsView.this, MainMenuView.class));
         }
@@ -44,6 +57,21 @@ public class SettingsView extends ActionBarActivity implements View.OnClickListe
         }
         else if (girlsRButton.isChecked()) {
             Constants.gameLayout = "girls";
+
+        }
+
+    }
+
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+        if (buttonView.getId() == R.id.cbSound) {
+
+            Constants.cbBooleanSound = isChecked;
+            Constants.amSound.setStreamMute(AudioManager.STREAM_MUSIC, !isChecked);
+        }
+        else if(buttonView.getId() == R.id.cbMusic){
+
+            Constants.cbBooleanMusic = isChecked;
+            Constants.amMusic.setStreamMute(AudioManager.STREAM_MUSIC, !isChecked);
 
         }
 
